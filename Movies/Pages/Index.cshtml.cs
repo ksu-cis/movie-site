a73fde623123d2fadd328bb0d4341b0cb25c4b87
Nathan Bean <nhbean@WIN2.CS.KSU.EDU>
2019-11-21 13:40:16 -0600

    changes for day 2

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -9,9 +9,6 @@ namespace Movies.Pages
 {
     public class IndexModel : PageModel
     {
-
-        public MovieDatabase MovieDatabase = new MovieDatabase();
-
         public List<Movie> Movies;
 
         [BindProperty]
@@ -30,24 +27,18 @@ namespace Movies.Pages
 
         public void OnPost()
         {
-            if (search != null && mpaa.Count > 0)
+            Movies = MovieDatabase.All;
+
+            if (search != null)
             {
-                Movies = MovieDatabase.Search(search);
-                Movies = MovieDatabase.FilterByMPAA(Movies, mpaa);
-            }
-            else if (search != null)
-            {
-                Movies = MovieDatabase.Search(search);
-            }
-            else if (mpaa.Count > 0)
-            {
-                Movies = MovieDatabase.FilterByMPAA(MovieDatabase.All, mpaa);
-            }
-            else
-            {
-                Movies = MovieDatabase.All;
+                Movies = MovieDatabase.Search(Movies, search);
             }
 
+            if (mpaa.Count > 0)
+            {
+                Movies = MovieDatabase.FilterByMPAA(Movies, mpaa);
+            }
+            
             if(minIMDB is float min)
             {
                 Movies = MovieDatabase.FilterByMinIMDB(Movies, min);
